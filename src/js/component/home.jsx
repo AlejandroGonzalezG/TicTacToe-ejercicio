97b247b67c9a36978a06af59d8d8a4fdc236df08
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-07 17:06:21 +0000

    avance tictactoe

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const estiloRow = {
+	display: "grid",
+    justifyContent: "center",
+	marginTop: "10px",
+	color: "white",
+	
+}
+
+const estiloContainer2 = {
+	textAlignt : "center",
+}
+
+const estiloH1 = {
+	justifyContent: "center",
+	display: "grid",
+	marginBottom: "50px"
+}
+
+const estiloH2 = {
+	display: "grid",
+	justifyContent: "center",
+	marginBottom: "30px",
+	color: "white",
+}
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row" style={estiloRow}>	
+			<div className="container2" style={estiloContainer2}>
+				<h1 className="titulo mx-auto">Tic Tac Toe in React.js</h1>
+					<h2 className="titulo2" style={estiloH1}>Pick a weapon!</h2>
+			</div>
+			<div>
+				<h2 style={estiloH2}>Choose your weapon</h2>
+				<input type="text" placeholder="Usarname Player 1"></input>
+				<input type="text" placeholder="Usarname Player 2"></input>
+				
+			</div>
+			<div className="container">
+				<button className="elegir1">X</button>
+				<button className="elegir2">O</button>
+			</div>		
 		</div>
 	);
 };
