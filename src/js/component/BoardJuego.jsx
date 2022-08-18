d16f69a55df0d2b4e17a76ae8807b666f0390f26
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-08-18 04:40:32 +0000

    avance boardjuego y ganador

--- src/js/component/BoardJuego.jsx
+++ src/js/component/BoardJuego.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const estiloRow = {
 	display: "grid",
@@ -147,31 +147,145 @@ const estiloColumna9 = {
 
 
 const BoardJuego = (props) => {
+	const [espacioUno, setEspacioUno] = useState("")
+	const [espacioDos, setEspacioDos] = useState("")
+	const [espacioTres, setEspacioTres] = useState("")
+	const [espacioCuatro, setEspacioCuatro] = useState("")
+	const [espacioCinco, setEspacioCinco] = useState("")
+	const [espacioSeis, setEspacioSeis] = useState("")
+	const [espacioSiete, setEspacioSiete] = useState("")
+	const [espacioOcho, setEspacioOcho] = useState("")
+	const [espacioNueve, setEspacioNueve] = useState("")
+	const [largo, setLargo] = useState([]);
+
+	const handleChangeUno = (e) => {
+		if (props.jugada != "O") {
+			setEspacioUno("X");
+			setLargo([...largo, "X"]);
+			props.setJugada("O");
+		} else {
+			setEspacioUno("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeDos = (e) => {
+		if (props.jugada != "O") {
+			setEspacioDos("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioDos("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeTres = (e) => {
+		if (props.jugada != "O") {
+			setEspacioTres("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioTres("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeCuatro = (e) => {
+		if (props.jugada != "O") {
+			setEspacioCuatro("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioCuatro("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeCinco = (e) => {
+		if (props.jugada != "O") {
+			setEspacioCinco("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioCinco("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeSeis = (e) => {
+		if (props.jugada != "O") {
+			setEspacioSeis("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioSeis("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeSiete = (e) => {
+		if (props.jugada != "O") {
+			setEspacioSiete("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioSiete("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeOcho = (e) => {
+		if (props.jugada != "O") {
+			setEspacioOcho("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioOcho("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+	const handleChangeNueve = (e) => {
+		if (props.jugada != "O") {
+			setEspacioNueve("X")
+			setLargo([...largo, "X"]);
+			props.setJugada("O")
+		} else {
+			setEspacioNueve("O")
+			setLargo([...largo, "O"]);
+			props.setJugada("X")
+		}
+	};
+
 	return (
 		<div className="row" style={estiloRow}>
 			<div className="container2" style={estiloContainer2}>
 				<h1 className="titulo mx-auto" style={estiloH1} >Tic Tac Toe in React.js</h1>
-				<h2 className="titulo2" style={estiloH1}>Its is {props.jugada} turn!</h2>
+				<h2 className="titulo2" style={estiloH1}>It's {props.jugada} turn!</h2>
 			</div>
+			{largo.length > 4 &&
+				((espacioUno == espacioDos && espacioUno == espacioTres && espacioUno != "") || (espacioCuatro == espacioCinco && espacioCuatro == espacioSeis && espacioCuatro != "") || (espacioSiete == espacioOcho && espacioSiete == espacioNueve && espacioSiete != "") || (espacioUno == espacioCuatro && espacioUno == espacioSiete && espacioCuatro != "") || (espacioDos == espacioCinco && espacioDos == espacioOcho && espacioDos != "") || (espacioTres == espacioSeis == espacioNueve && espacioTres != "") || (espacioUno == espacioCinco && espacioUno == espacioNueve && espacioNueve != "") || (espacioTres == espacioCinco && espacioTres == espacioSiete && espacioSiete != "")) ? (<h1>¡Haz Ganado!</h1>) : null
+			}
 			<div className="container text-center">
 				<div className="row">
-					<div className="col-4" style={estiloColumna1}></div>
-					<div className="col-4" style={estiloColumna2}></div>
-					<div className="col-4" style={estiloColumna3}></div>
+					<div className="col-4" style={estiloColumna1} onClick={(e) => handleChangeUno(e)}>{espacioUno}</div>
+					<div className="col-4" style={estiloColumna2} onClick={(e) => handleChangeDos(e)}>{espacioDos}</div>
+					<div className="col-4" style={estiloColumna3} onClick={(e) => handleChangeTres(e)}>{espacioTres}</div>
 				</div>
 			</div>
 			<div className="container text-center">
 				<div className="row">
-					<div className="col-4" style={estiloColumna4}></div>
-					<div className="col-4" style={estiloColumna5}></div>
-					<div className="col-4" style={estiloColumna6}></div>
+					<div className="col-4" style={estiloColumna4} onClick={(e) => handleChangeCuatro(e)}>{espacioCuatro}</div>
+					<div className="col-4" style={estiloColumna5} onClick={(e) => handleChangeCinco(e)}>{espacioCinco}</div>
+					<div className="col-4" style={estiloColumna6} onClick={(e) => handleChangeSeis(e)}>{espacioSeis}</div>
 				</div>
 			</div>
 			<div className="container text-center">
 				<div className="row">
-					<div className="col-4" style={estiloColumna7}></div>
-					<div className="col-4" style={estiloColumna8}></div>
-					<div className="col-4" style={estiloColumna9}></div>
+					<div className="col-4" style={estiloColumna7} onClick={(e) => handleChangeSiete(e)}>{espacioSiete}</div>
+					<div className="col-4" style={estiloColumna8} onClick={(e) => handleChangeOcho(e)}>{espacioOcho}</div>
+					<div className="col-4" style={estiloColumna9} onClick={(e) => handleChangeNueve(e)}>{espacioNueve}</div>
 				</div>
 			</div>
 		</div>
